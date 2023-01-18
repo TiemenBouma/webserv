02416e193fb3f2c48357ddb9ac13773c3da15089
CookieMonsterXL <88137591+CookieMonsterXL@users.noreply.github.com>
2023-01-18 11:40:02 +0100

    implemented select

--- serv_example.cpp
+++ serv_example.cpp
@@ -11,42 +11,27 @@
 #include <unistd.h>
 
 const int PORT = 8080;
-const int MAX_CONNECTIONS = 5;
+const int MAX_CONNECTIONS = FD_SETSIZE - 1;
 const int BUFFER_SIZE = 1024;
+typedef struct sockaddr_in SA_IN;
+typedef struct sockaddr SA;
+
+int	init_server(int port, int backlog);
+int	accept_new_connection(int server_sock);
+void handle_connection(int client_socket);
 
 int main(int argc, char* argv[]) {
-    int sockfd, new_sockfd;
-    struct sockaddr_in server_addr, client_addr;
+    int server_socket, client_socket;
+    SA_IN server_addr; 
+	SA_IN client_addr;
     int client_len;
-    char buffer[BUFFER_SIZE];
 
-    // create socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        std::cerr << "Error: " << strerror(errno) << std::endl;
-        return 1;
-    }
 
-    // set server address
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    // bind socket to address
-    if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-        std::cerr << "Error: " << strerror(errno) << std::endl;
-        return 2;
-    }
-
-    // listen for connections
-    if (listen(sockfd, MAX_CONNECTIONS) < 0) {
-        std::cerr << "Error: " << strerror(errno) << std::endl;
-        return 3;
-    }
+	server_socket = init_server(PORT, MAX_CONNECTIONS);
 
 	fd_set current_sockets, ready_sockets;
 	FD_ZERO(&current_sockets);
-	FD_SET(sockfd, &current_sockets);
+	FD_SET(server_socket, &current_sockets);
 
     // handle connections
     while (true) {
@@ -57,30 +42,70 @@ int main(int argc, char* argv[]) {
 		}
 		for (int i = 0; i < FD_SETSIZE; i++) {
 			if (FD_ISSET(i, &ready_sockets)) {
-				if (i = sockfd) {
-					
+				if (i == server_socket) {
+					//this is a new connection that we can accept
+					client_socket = accept_new_connection(server_socket);
+					FD_SET(client_socket, &current_sockets);
+				} else {
+					handle_connection(i);
+					FD_CLR(i, &current_sockets);
 				}
 			}
 		}
-        client_len = sizeof(client_addr);
-        new_sockfd = accept(sockfd, (struct sockaddr *) &client_addr, (socklen_t *) &client_len);
-        if (new_sockfd < 0) {
-            std::cerr << "Error: " << strerror(errno) << std::endl;
-            return 4;
-        }
+    }
+    return 0;
+}
 
-        // read request
+int	init_server(int port, int backlog) {
+	int server_socket, client_socket;
+    SA_IN server_addr; 
+
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        std::cerr << "Error: " << strerror(errno) << std::endl;
+        return 1;
+    }
+
+	 // set server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // bind socket to address
+    if (bind(server_socket, (SA *) &server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "Error: " << strerror(errno) << std::endl;
+        return 2;
+    }
+
+    // listen for connections
+    if (listen(server_socket, MAX_CONNECTIONS) < 0) {
+        std::cerr << "Error: " << strerror(errno) << std::endl;
+        return 3;
+    }
+	return server_socket;
+}
+
+int	accept_new_connection(int server_sock) {
+	int addr_len = sizeof(SA_IN);
+	int client_socket;
+	SA_IN client_addr;
+	client_socket = accept(server_sock, (SA *) &client_addr, (socklen_t *) &addr_len);
+	if (!client_socket)
+		exit(1);
+	return client_socket;
+}
+
+void handle_connection(int client_socket) {
+	    char buffer[BUFFER_SIZE];
+	        // read request
         memset(buffer, 0, BUFFER_SIZE);
-        read(new_sockfd, buffer, BUFFER_SIZE - 1);
+        read(client_socket, buffer, BUFFER_SIZE - 1);
         std::cout << "Received request: " << buffer << std::endl;
 		write(1, buffer, strlen(buffer));
 		std::cout << std::endl;
 
         // send response
         std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello, World!";
-        write(new_sockfd, response.c_str(), response.size());
-        close(new_sockfd);
-    }
-
-    return 0;
+        write(client_socket, response.c_str(), response.size());
+        close(client_socket);
 }
