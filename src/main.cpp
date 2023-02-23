e60646a2812b9c960f1838cc3fe8fd62020a6da1
Sue <54895147+SwenneHacks@users.noreply.github.com>
2023-02-23 13:49:36 +0100

    clean errors

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,10 @@ int	main(int argc, char *argv[])
 	std::string	fullInput;
 	std::vector<ConfigServer> servers;
 
-	// this checks the state of the configFile ifstream.
-	// std::cout << configFile.good() << std::endl;
-	// std::cout << configFile.eof() << std::endl;
-	// std::cout << configFile.fail() << std::endl;
-	// std::cout << configFile.bad() << std::endl;
 	// [INFO] server needs a config file
 	if (argc != 2)
-		error_msg("Expected: ./webserv config_file", EXIT_FAILURE);
+		error_msg("\nExpected: ./webserv config_file", EXIT_FAILURE);
+	
     while (std::getline(configFile, line)) {
 	//	std::cout << line << std::endl;
 		fullInput += line;
